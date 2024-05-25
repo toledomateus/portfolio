60b46ed857a4fafef9360ddd20ec9832cfe727f5
Mateus Toledo <mateustsleao@gmail.com>
2024-05-25 16:19:41 +0100

    add exp, proojects, contact sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,89 @@
-
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /* Menu show */
 if (navToggle) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu')
-  })
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /* Menu hidden */
 if (navClose) {
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu')
-  })
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
-
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-  const navMenu = document.getElementById('nav-menu')
+  const navMenu = document.getElementById("nav-menu");
   // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SWIPER PROJECTS ===============*/
 
+let swiperProjects = new Swiper(".projects__container", {
+  loop: true,
+  spaceBetween: 24,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: -56,
+    }
+  },
+});
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
-
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactSubject = document.getElementById('contact-subject'),
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+  e.preventDefault()
+
+  const isEmpty = contactName.value === '' ||
+    contactEmail.value === '' ||
+    contactSubject.value === ''
+  if(isEmpty) {
+
+    //Change to warning message
+    contactMessage.classList.remove('color-blue')
+    contactMessage.classList.add('color-red')
+
+    contactMessage.textContent = 'Write all the input fields 📩'
+
+  } else {
+    //Service ID = service_z9xyrpn
+    emailjs.sendForm('service_z9xyrpn','','')
+  }
 
 
+}
+contactForm.addEventListener('submit', sendEmail)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SHOW SCROLL UP ===============*/
 
-
 /*=============== DARK LIGHT THEME ===============*/
 
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
